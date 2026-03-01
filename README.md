356a089d84ccc64522b4604c0a14544b04d4b9a3
hugolscunha <hugoleonardoscunha@gmail.com>
2026-03-01 15:47:06 -0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Tripleten web_project_around_pt
+# Tripleten web_project_around
+
+**Around The Brazil** é projeto do curso de Desenvolvimento Web da TripleTen. Uma página web interativa que permite visualizar, adicionar  uma galeria de fotos de lugares visitados pelo Brasil. 
+
+O utilizador pode interagir com as fotos, com funcionalidades para adicionar, remover e curtir as imagens (ainda não implementados neste estado) .
+
+## Tecnologias Utilizadas
+- HTML
+- CSS
+  - @font-face
+  - Layouts responsivos 
+- JavaScript
+    - Interatividade
+    - Manipulação do DOM
+
+- Recursos de interatividade com JavaScript:
+    - Abertura e fechamento de um modal de edição de perfil.
+    - Atualização dinâmica do nome e da descrição do perfil.
+    - Fechamento do modal ao pressionar a tecla "Esc" ou clicar fora da janela do modal.
+    - Validação de formulários.
+
+- Figma
+  - Todo desenvolvimento foi feito com base em layouts feitos no Figma.
+
+## Melhorias Futuras
+- **Persistência de dados:** Atualmente, as alterações (como curtir fotos ou adicionar/remover) não são persistentes. A ideia é integrar um mecanismo de armazenamento local (Local Storage) ou um backend para guardar as interações do utilizador.
+- **Acessibilidade:** Garantir que a página seja acessível a todos os utilizadores. Isso pode envolver melhorias no HTML, CSS (garantindo contraste adequado) e no JavaScript (tornando as interações acessíveis via teclado).
+- **Modularização do JavaScript:** Organizar o código em módulos para melhor manutenção e escalabilidade.
---
+++ scripts/index.js
@@ -0,0 +1,224 @@
+//SEÇÃO PROFILE EDIT
+
+// Variáveis da sessão profile
+const editProfilePopup = document.querySelector("#profile"); // Variável da janela popup de edição de perfil(hidden)
+const editProfileButton = document.querySelector("#edit-button"); // Botão com simbolo de caneta para abrir popup de edição
+const closeEditButton = editProfilePopup.querySelector(".popup__button_type_close"); // Botão de fechar o popup de edição de perfil
+const formElement = editProfilePopup.querySelector(".popup__form"); // Variável do formulário (tag form) do popup de edição de perfil
+const submitButton = formElement.querySelector(".popup__button_type_send"); // Variável do botão de enviar do popup de edição de perfil
+
+const profileInputs = formElement.querySelectorAll(".popup__item"); // Variável de todos os inputs do popup de edição de perfil
+const nameInput = formElement.querySelector(".popup__item_type_name"); // Variável do input de nome do popup de edição de perfil
+const aboutInput = formElement.querySelector(".popup__item_type_about"); // Variável do input de descrição do popup de edição de perfil
+
+const profileName = formElement.querySelector(".profile__name"); // Variável do nome do perfil (o que vai aparecer no perfil depois de editado)
+const profileAbout = formElement.querySelector(".profile__description"); // Variável da descrição do perfil (o que vai aparecer no perfil depois de editado)
+
+// Funções de fechar popup de edição de perfil
+function closeProfilePopup() {
+  editProfilePopup.classList.remove("popup__opened");
+}
+
+// EventListeners de abrir popup de edição de perfil
+editProfileButton.addEventListener("click", () => {
+  editProfilePopup.classList.add("popup__opened");
+  toggleButtonState(profileInputs, submitButton); // Verifica o estado do botão de enviar ao abrir o popup para garantir que ele esteja desabilitado se os campos estiverem vazios
+});
+
+// EventListener de fechar popup de edição de perfil
+closeEditButton.addEventListener("click", closeProfilePopup);
+
+// Verfica os inputs a cada digitação para mostrar ou esconder o span de erro e controlar o botão de enviar do popup de edição de perfil
+ profileInputs.forEach((input)=> {
+  input.addEventListener("input", () => {
+    validateInput(formElement, input);
+    toggleButtonState(profileInputs, submitButton);
+  });
+});
+
+// Adiciona evento de submissão do formulário de perfil para alterar o perfil
+formElement.addEventListener("submit", (evt) => {
+  evt.preventDefault(); // Previne o comportamento padrão de recarregar a página ao enviar o formulário
+  profileName.textContent = nameInput.value;
+  profileAbout.textContent = aboutInput.value;
+  closeProfilePopup();
+});
+
+//----------------------------------------------------------------------------------
+
+//SEÇÃO ADD CARD
+
+// Variáveis da sessão add-card
+const addPopup = document.querySelector("#add-popup"); // Variável da janela de adicionar card (hidden)
+const addButton = document.querySelector("#add-button"); // Botão de adicionar card (+)
+const closeAddButton = addPopup.querySelector(".popup__button_type_close"); // Botão de fechar popup addcard
+const addForm = document.querySelector("#add-form"); // Variável do formulário (tag form) do popup de adição de card
+
+const submitAddButton = addForm.querySelector(".popup__button_type_send"); // Variável do botão de enviar do popup de adição de novo lugar
+
+const addInputs = addForm.querySelectorAll(".popup__item"); // Variável de todos os inputs do popup de adição de novo lugar
+const localNameInput = addForm.querySelector(".popup__item_type_name"); // Variável do input de nome do popup de adição de novo lugar
+const linkInput = addForm.querySelector(".popup__item_type_link"); // Variável do input de link da imagem do popup de adição de novo lugar
+
+// Função de fechar popup de adição de card
+function closeAddPopup() {
+  addPopup.classList.remove("popup__opened");
+};
+
+// EventListener de abrir o popup de adição de card
+addButton.addEventListener("click", () => {
+  addPopup.classList.add("popup__opened");
+  toggleButtonState(addInputs, submitAddButton); // Verifica o estado do botão de enviar ao abrir o popup para garantir que ele esteja desabilitado se os campos estiverem vazios
+});
+
+// EventListener de fechar popup de adição de card
+closeAddButton.addEventListener("click", closeAddPopup);
+
+// Verfica os inputs a cada digitação para mostrar ou esconder o span de erro e controlar o botão de enviar do popup de edição de perfil
+ addInputs.forEach((input)=> {
+  input.addEventListener("input", () => {
+    validateInput(addForm, input);
+    toggleButtonState(addInputs, submitAddButton);
+  });
+});
+
+// Evento de submissão do formulário de adição de card para criar um novo card
+addForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+const newCard = createCard(localNameInput.value, linkInput.value);
+cardArea.prepend(newCard);
+
+addForm.requestFullscreen(); // Reseta o formulário para limpar os campos e o estado do botão de enviar
+toggleButtonState(addInputs, submitAddButton); // Reseta o estado do botão de enviar para desabilitado
+closeAddPopup();
+});
+
+//-------------------------------------------------------------------------------------
+
+//SESSÃO ELEMENTES
+
+// Cards iniciais (objeto com as chaves de cada card inicial)
+const initialCards = [
+  {
+    name: "Fernando de Noronha",
+    link: "https://images.pexels.com/photos/11159713/pexels-photo-11159713.jpeg",
+  },
+  {
+    name: "Gramado",
+    link: "https://images.pexels.com/photos/18171957/pexels-photo-18171957.jpeg",
+  },
+  {
+    name: "Manaus",
+    link: "https://images.pexels.com/photos/29759408/pexels-photo-29759408.jpeg",
+  },
+  {
+    name: "Rio de Janeiro",
+    link: "https://images.pexels.com/photos/1458036/pexels-photo-1458036.jpeg",
+  },
+  {
+    name: "Salvador",
+    link: "https://images.pexels.com/photos/31792594/pexels-photo-31792594.jpeg",
+  },
+  {
+    name: "São Paulo",
+    link: "https://images.pexels.com/photos/72479/pexels-photo-72479.jpeg",
+  },
+];
+
+// Seleciona o template e o container de cards
+const template = document.querySelector(".elements__template"); // Variável de toda seção do template
+const cardArea = document.querySelector(".elements__cards"); // variavel de card inicial (um único)
+
+// Função para criar novo card 
+function createCard(name, link) {
+  //Variavel  que pega o conteúdo do card para criação de novas variaveis dos elementos dentro do card
+  const cardElement = template.content.querySelector(".elements__card").cloneNode(true); 
+
+  // Variáveis criadas para cada elemento do card
+  const cardImage = cardElement.querySelector(".elements__image"); // Seleciona imagem do card
+  const cardText = cardElement.querySelector(".elements__text"); // Seleciona texto do card
+  const cardTrashButton = cardElement.querySelector(".elements__button_type_trash"); // Seleciona botão de lixeira
+  const cardLikeButton = cardElement.querySelector(".elements__button_type_like"); // Seleciona botão de like
+  const cardWindow = cardElement.querySelector(".elements__window");  // Seleciona 
+  const buttonCardImage = cardElement.querySelector(".elements__image-button"); // Seleciona o botão que envolve a imagem para abri-la
+  const cardWindowImage = cardElement.querySelector(".elements__window-image"); // Seleciona imagem aberta
+  const cardWindowName = cardElement.querySelector(".elements__window-name"); // Seleciona texto da imagem quando aberta
+  const windowCloseButton = cardElement.querySelector("#window-close-button"); // Seleciona botão de fechar da imagem aberta
+
+  // Define conteúdo do card usando o objeto initial cards
+  cardImage.src = link;
+  cardImage.alt = name;
+  cardText.textContent = name;
+  cardWindowImage.src = link;
+  cardWindowImage.alt = name;
+  cardWindowName.textContent = name;
+
+  // Evento de deletar um card
+  cardTrashButton.addEventListener("click", () => cardElement.remove());
+
+  // Evento de curtir
+  cardLikeButton.addEventListener("click", () => {
+    cardLikeButton.classList.toggle("elements__button_type_like-active");
+  });
+
+  // Abrir janela de imagem
+  buttonCardImage.addEventListener("click", () => {
+    cardWindow.classList.add("elements__window_opened");
+  });
+
+  // Fechar janela de imagem
+  windowCloseButton.addEventListener("click", () => {
+    cardWindow.classList.remove("elements__window_opened");
+  });
+
+  return cardElement;
+}
+
+// Adiciona os cartões iniciais
+initialCards.forEach(({ name, link }) => {
+  const card = createCard(name, link);
+  cardArea.append(card);
+});
+//------------------------------------------
+
+// FUNÇÕES COMUNS PARA OS DOIS FORMULARIOS DE POPUP (EDITAR PERFIL E ADICIONAR CARD)
+
+// COMUM  Validação dos inputs para ver se mostra o span de erro ou não (chamado pelo forEach dos inputs)
+function validateInput(form, input) {
+  const errorElement = form.querySelector(`.popup__error_type_${input.name}`);
+  errorElement.textContent = input.validity.valid ? "" : input.validationMessage;
+};
+
+// COMUM Verifica se tem algum input inválido para controlar o botão de enviar do popup de edição de perfil
+function hasInvalidInput(inputList) {
+ return Array.from(inputList).some(input => !input.validity.valid);
+};
+
+// COMUM Controla botão de enviar do popup de edição de perfil (chamado pelo forEach dos inputs)
+function toggleButtonState(inputList, buttonElement) {
+  buttonElement.disabled = hasInvalidInput(inputList);
+};
+
+// COMUM Fecha o popup ao clicar no overlay (chamado pelos eventListeners de cada popup)
+function handleOverlayClick(evt) {
+  if (evt.target.classList.contains("popup")) {
+    evt.target.classList.remove("popup__opened");
+  }
+}
+
+editProfilePopup.addEventListener("mousedown", handleOverlayClick);
+addPopup.addEventListener("mousedown", handleOverlayClick);
+
+// COMUM Fecha o popup ao clicar na tecla Esc (chamado pelo eventListener de toda a página)
+function handleEscClose(evt) {
+  if (evt.key === "Escape") {
+    document.querySelectorAll(".popup__opened").forEach(popup => {
+      popup.classList.remove("popup__opened");
+    });
+  }
+}
+
+document.addEventListener("keydown", handleEscClose);
+
+//----------------------------------------------------------------------------------
